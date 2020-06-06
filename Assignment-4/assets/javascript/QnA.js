447a07f86e83eac18d4d0091d05808b4100cc3ad
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-06-06 23:01:45 +1000

    update assignment 4

--- Assignment-4/assets/javascript/QnA.js
+++ Assignment-4/assets/javascript/QnA.js
@@ -16,54 +16,119 @@ var questions = [
 
 //Question 2
     {
-      questionsTitle: "Commonly used data types DO NOT include:",
+      questionsTitle: "Inside which HTML element do we put the JavaScript?",
       
       multiChoice: [
-          "strings", 
-          "booleans", 
-          "alerts", 
-          "numbers"],
+          "<script>", 
+          "<scripting>", 
+          "<javascript>", 
+          "<js>"],
 
-      correctAns: "alerts"
+      correctAns: "<script>"
     },
 
 //Question 3
     {
-      questionsTitle: "Commonly used data types DO NOT include:",
+      questionsTitle: "Where is the correct place to insert a JavaScript?",
       
       multiChoice: [
-          "strings", 
-          "booleans", 
-          "alerts", 
-          "numbers"],
+          "The <head> section", 
+          "The <body> section", 
+          "The <nav> section", 
+          "Both the <head> section and the <body> section are correct"],
 
-      correctAns: "alerts"
+      correctAns: "Both the <head> section and the <body> section are correct"
     },
 
 //Question 4
     {
-      questionsTitle: "Commonly used data types DO NOT include:",
+      questionsTitle: "What is the correct syntax for referring to an external script called xxx.js?",
       
       multiChoice: [
-          "strings", 
-          "booleans", 
-          "alerts", 
-          "numbers"],
+          "<script src ='xxx.js'>", 
+          "<script href = 'xxx.js'", 
+          "<script name = 'xxx.js'", 
+          "<script alt = 'xxx.js'"],
 
-      correctAns: "alerts"
+      correctAns: "<script src ='xxx.js'>"
     },
     
 //Question 5
     {
-      questionsTitle: "Commonly used data types DO NOT include:",
+      questionsTitle: "How do you write 'Hello World' in an alert box?",
       
       multiChoice: [
-          "strings", 
-          "booleans", 
-          "alerts", 
-          "numbers"],
+          "Hello;", 
+          "alert('hello');", 
+          "alertBox('hello');", 
+          "msg('hello');"],
 
-      correctAns: "alerts"
+      correctAns: "alert('hello');"
     },
+
+//Question 6
+    {
+      questionsTitle: "How do you create a function in JavaScript?",
+      
+      multiChoice: [
+          "creat function()", 
+          "function:myFunction()", 
+          "function = myfunction()", 
+          "function myFunction()"],
+
+      correctAns: "function myFunction()"
+    },
+
+    //Question 7
+    {
+      questionsTitle: "How to write an IF statement in JavaScript?",
+      
+      multiChoice: [
+          "if (i === 4)", 
+          "if 4 === i", 
+          "if i === 4 then", 
+          "else if i==4 then"],
+
+      correctAns: "if (i === 4)"
+    },
+
+    //Question 8
+    {
+      questionsTitle: "How does a WHILE loop start?",
+      
+      multiChoice: [
+          "if while (i <= 10; i++)", 
+          "while (i <=10)", 
+          "while i = 1 to 10", 
+          "while (i <= 10; i++)"],
+
+      correctAns: "while (i <=10)"
+    },
+
+    //Question 9
+    {
+      questionsTitle: "How can you add a comment in a JavaScript?",
+      
+      multiChoice: [
+          "<!-- comment -->", 
+          "// comment", 
+          "/* comment */", 
+          "'comment'"],
+
+      correctAns: "// comment"
+    },
+
+    //Question 10
+    {
+      questionsTitle: "How do you round the number 7.25, to the nearest integer?",
+      
+      multiChoice: [
+          "round(7.25)", 
+          "rnd(7.25)", 
+          "Math.round(7.25)", 
+          "Math.rnd(7.25)"],
+
+      correctAns: "Math.round(7.25)"
+    }
   ];
   

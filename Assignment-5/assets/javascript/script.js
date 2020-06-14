b22a8ade1bb4102c78a7e1bd9d09dab6cfd95d01
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-06-14 22:16:57 +1000

    final js and html

--- Assignment-5/assets/javascript/script.js
+++ Assignment-5/assets/javascript/script.js
@@ -1,151 +1,99 @@
-//Logic for Quiz Game
+    //moment object
+    var moment = moment();
 
-//Variables
-var time = 60;
-var timeLeft;
-var questionsList = 0;
+    //Display day, month, date, year
+    $("#headerDate").text((moment.format("dddd, MMMM Do YYYY")));
 
-//Tone for correct & incorrect
-var correctSound = new Audio("assets/audio/correct.mp3");
-var incorrectSound = new Audio("assets/audio/incorrect.mp3")
+    //start time 9am
+    var startTime = 9;
+    //Calculate current time to the hour
+    var currentTime = moment.hour();
+    //duration from start time to current time
+    var timeDiff = currentTime - startTime;
 
-//When start Quiz button is clicked hide the start instructions
-
-$("#startQuiz").on("click", function () {
-    $(".main").hide();
-
-    //Start timer
-    timeLeft = setInterval(timeLapse, 1000);
-
-    //Start Quiz function 
-    startQuestions();
-
-});
-
-//Function for time ticking down
-function timeLapse() {
-    $("#timer").text(time);
-    time--;
+    // Clear text Area if past 6pm
+      if (timeDiff > 9) {
+      localStorage.clear()
+    };
     
-    if (time <= 0) {
-        finQuiz();
-    } 
-};
+    // past, present, future color coordination
+    $(".form-control").each(function () {
 
-//Display Questions
-function startQuestions() {
-
-    //Get question index of 0
-    var showQ = questions[questionsList];
-
-    //Render Question
-    var titleQ = $("#quizHeading").text(showQ.questionsTitle);
-    titleQ.textContent = questionsList.questionsTitle;
-
-    //Clear html questions choice 
-    $("#multiChoice").text("");
-
-    //Render Multiple Choices
-    showQ.multiChoice.forEach(function (multiChoice, i) {
-
-        //Render buttons
-        var multiButton = document.createElement("button");
-        multiButton.setAttribute("class", "choice");
-        multiButton.setAttribute("value", multiChoice);
-
-        multiButton.textContent = i + 1 + ". " + multiChoice;
-
-        //addEvent Listener for multiple choice
-        multiButton.onclick = (multiSelect);
-
-        //Append multiple choice to page
-        $("#multiChoice").append(multiButton);
+      if (timeDiff === ($(this).data("columns"))) {
+        $(this).css("background-color", "palevioletred");
+      } else if (($(this).data("columns")) < timeDiff) {
+        $(this).css("background-color", "lightgrey");
+      } else if (($(this).data("columns")) > timeDiff) {
+        $(this).css("background-color", "darkseagreen");
+      }
 
     });
-}
 
-//Multiple choice selection Logic
-function multiSelect() {
+    //Get tasks from local storage
+    var getUserTasks = JSON.parse(localStorage.getItem("userTasks"));
 
-    if (this.value !== questions[questionsList].correctAns) {
-        //minus 10 seconds 
-        time -= 10;
+    // If local storage is null create time block object
+    if (getUserTasks !== null) {
 
-        //if out of time
-        if (time < 0) {
-            time = 0;
+      for (var i=0; i < getUserTasks.timeBlocks.length; i++) {
+      $(".form-control").eq(i).html(getUserTasks.timeBlocks[i]);
+      
+      timeBlockTasks = getUserTasks;
+
+      }
+    } else {
+
+      var timeBlockTasks = {
+          timeBlocks: new Array(9)
+      };
+
+    };
+
+    //Event listener for text area to display Modal
+    $(".form-control").on("click", function () {
+      $('#myModal').modal('show');
+
+      //get block column value
+      var $blockId = $(".form-control").index(this);
+
+      $("#blockId").text($blockId);
+
+    });
+
+    //Save button function 
+    $("#saveButton").on("click", function () {
+
+      var $taskInput = $("#taskInput");
+
+      var blockId = $("#blockId").text();
+
+      //Access Array for time block user input
+      var timeBlockArr = timeBlockTasks.timeBlocks;
+
+      for (var i = 0; i < timeBlockArr.length; i++) {
+
+        if (i == blockId) {
+
+          timeBlockArr[i] = $taskInput.val().trim();
+
+          break;
         }
+      }
 
-        //decreased new time
-        $("#timer").text(time);
+      //stringify user input
+      var timeBlockTasks_serialized = JSON.stringify(timeBlockTasks);
 
-        //Show, hide alert, play sound
-        incorrectSound.play();
-        $(".alert-danger").attr("hidden", false);
+      //save to local storage
+      localStorage.setItem("userTasks", timeBlockTasks_serialized);
 
-        setTimeout(function () {
-            $(".alert-danger").attr("hidden", true);
-        }, 1000);
+      //Display Array value to textarea 
+      var indexValue = timeBlockArr[blockId];
 
-    } else {
+      $(".form-control").eq(blockId).html(indexValue);
 
-        correctSound.play();
-        $(".alert-success").attr("hidden", false);
+      // //clear text
+      $taskInput.val("");
 
-        setTimeout(function () {
-            $(".alert-success").attr("hidden", true);
-        }, 500);
-    }
+      $('#myModal').modal('hide');
 
-    //Next Question in the List
-    questionsList++;
-
-    //if end of questions list
-    if (questionsList === questions.length) {
-        finQuiz();
-    } else {
-        startQuestions();
-    }
-}
-
-function finQuiz() {
-    //stop timer count down
-    clearInterval(timeLeft);
-
-    //show Game over screen 
-    $("#gameOver").attr("hidden", false);
-
-    //Display score
-    var userScore = $("#userScore");
-    userScore.text(time);
-
-    //Hide Questions
-    $("#questions").hide();
-}
-
-//Saving User High Score
-function storeHS() {
-    var userInitials = $("#initials").val();
-  
-    //block null input for initials
-    if (userInitials !== "") {
-        var userHS = JSON.parse(window.localStorage.getItem("highScoreList")) || [];
-
-        var score = {
-            score: time,
-            initials: userInitials
-        };
-
-        //save to local storage
-        userHS.push(score);
-        window.localStorage.setItem("highScoreList", JSON.stringify(userHS));
-
-        // redirect to next page
-        window.location.href = "HighScore.html";
-    }
-}
-
-//Click submit initials button
-$("#submit").on("click", storeHS);
-
-
+    });
